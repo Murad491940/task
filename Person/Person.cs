ebc06c86cbaf96f0009f564c7c589d3afa961186
Murad491940 <84373217+Murad491940@users.noreply.github.com>
2023-06-02 23:22:56 +0400

    homework2

---
+++ Person/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    class Person
+    {
+        public string Name;
+        public string SurName;
+        public int Age;
+        public string Weight;
+        public string Height;
+
+        public Person(string aName, string aSurName, int aAge, string aWeight, string aHeight)
+        {
+            this.Name = aName;
+            this.SurName = aSurName;
+            this.Age = aAge;
+            this.Weight = aWeight;
+            this.Height = aHeight; 
+        }
+
+      
+
+
+        public override string ToString()
+        {
+            return $"Your name is {Name} your surname is {SurName} your age is {Age} your weight is {Weight} your height is {Height}!";
+        }
+
+
+    }
+}
---
+++ Person/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.CompilerServices;
+using System.Xml.Linq;
+
+namespace Person
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           List<Person> persons = new ();
+
+            Console.WriteLine("Enter name:");
+            string aName = Console.ReadLine();
+            Console.WriteLine("Enter surname:");
+            string aSurName = Console.ReadLine();
+            Console.WriteLine("Enter age:");
+            int aAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter weight:");
+            string aWeight = Console.ReadLine();
+            Console.WriteLine("Enter height:");
+            string aHeight = Console.ReadLine();
+            
+
+            var person = new Person(aName, aSurName, aAge, aWeight, aHeight);
+
+
+           Console.WriteLine(person.ToString());
+            
+
+
+
+
+
+        }
+    }
+}
